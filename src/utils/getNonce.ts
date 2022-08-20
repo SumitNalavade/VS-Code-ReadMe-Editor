d64ede1b1ddcab1d8aa4fde788033a3fe02ff85a
unjinjang <alpsboy1996@gmail.com>
2022-08-20 09:47:50 +0900

    [REFACTOR]: Seperate utility function into seperate folder

---
+++ src/utils/getNonce.ts
@@ -0,0 +1,13 @@
+function getNonce(): string {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+export default getNonce;
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as getNonce } from "./getNonce";
